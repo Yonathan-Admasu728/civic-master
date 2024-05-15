ab6d7fe3f8ac59250570e7a462f0fd3b8fe86313
Yonathan  Admasu <admyoni4@gmail.com>
2024-05-15 09:55:26 -0700

    simplified the nextAuth and the monodbconnect

--- libs/mongo.js
+++ libs/mongo.js
@@ -1,33 +1,27 @@
 import { MongoClient } from "mongodb";
 
-// This lib is use just to connect to the database in next-auth.
-// We don't use it anywhere else in the API routes—we use mongoose.js instead (to be able to use models)
-// See /libs/nextauth.js file.
-
-const uri = process.env.MONGODB_URI;
-const options = {};
-
 let client;
 let clientPromise;
 
-if (!uri) {
-  console.group("⚠️ MONGODB_URI missing from .env");
-  console.error(
-    "It's not mandatory but a database is required for Magic Links."
-  );
-  console.error(
-    "If you don't need it, remove the code from /libs/next-auth.js (see connectMongo())"
-  );
-  console.groupEnd();
-} else if (process.env.NODE_ENV === "development") {
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your MongoDB URI to .env.local");
+}
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(process.env.MONGODB_URI, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(process.env.MONGODB_URI, options);
   clientPromise = client.connect();
 }
 
 export default clientPromise;
+
--- libs/next-auth.js
+++ libs/next-auth.js
@@ -5,11 +5,9 @@ import config from "../config";
 import connectMongo from "./mongo";
 
 export const authOptions = {
-  // Set any random key in .env.local
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
-      // Follow the "Login with Google" tutorial to get your credentials
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
       async profile(profile) {
@@ -22,8 +20,6 @@ export const authOptions = {
         };
       },
     }),
-    // Follow the "Login with Email" tutorial to set up your email server
-    // Requires a MongoDB database. Set MONOGODB_URI env variable.
     ...(connectMongo
       ? [
           EmailProvider({
@@ -33,11 +29,7 @@ export const authOptions = {
         ]
       : []),
   ],
-  // New users will be saved in Database (MongoDB Atlas). Each user (model) has some fields like name, email, image, etc..
-  // Requires a MongoDB database. Set MONOGODB_URI env variable.
-  // Learn more about the model type: https://next-auth.js.org/v3/adapters/models
   ...(connectMongo && { adapter: MongoDBAdapter(connectMongo) }),
-
   callbacks: {
     session: async ({ session, token }) => {
       if (session?.user) {
@@ -51,8 +43,6 @@ export const authOptions = {
   },
   theme: {
     brandColor: config.colors.main,
-    // Add you own logo below. Recommended size is rectangle (i.e. 200x50px) and show your logo + name.
-    // It will be used in the login flow to display your logo. If you don't add it, it will look faded.
-    // logo: `https://${config.domainName}/logoAndName.png`,S
   },
+  debug: true, // Enable debug mode for detailed logs
 };
