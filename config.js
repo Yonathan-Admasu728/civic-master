3683944b691da850c1c180e30bd43e433cb2686e
Yonathan  Admasu <admyoni4@gmail.com>
2024-05-16 02:52:04 -0700

    modified the config file

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
   appDescription:
   "Empowering Your Path to Success with Innovative Learning Solutions",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "civicmaster.org",
+  domainName: "www.civicmaster.org",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
     id: "",
